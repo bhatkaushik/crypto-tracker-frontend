874629829e4bee43ff8f7a30890076a18a0c7f47
Kaushik G Bhat <kaushikgbhat@Kaushiks-Mac-mini.local>
2022-12-31 00:25:31 +0530

    completed navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import cryptoLogo from "../../assets/Group 33648.png";
 import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src={cryptoLogo} alt="navbar-logo" width="18" />
-      <h5>Crypto Tracker</h5>
-      <div>
+      <a className="navbar-brand" href="#">
+        <img
+          src={cryptoLogo}
+          width={28}
+          height={30}
+          className="d-inline-block align-top"
+          alt="logo"
+        />
+        {"  "} Crypto Tracker
+      </a>
+      <div className="navitemWrapper d-flex">
+
+      <div class="nav-item active search nav-push-right">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width={18}
@@ -14,9 +25,11 @@ const Navbar = () => {
           fill="currentColor"
           className="bi bi-search"
           viewBox="0 0 16 16"
-        >
+          >
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
         </svg>
+      </div>
+      <div class="nav-item active">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width={20}
@@ -24,13 +37,14 @@ const Navbar = () => {
           fill="currentColor"
           className="bi bi-list"
           viewBox="0 0 16 16"
-        >
+          >
           <path
             fillRule="evenodd"
             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-          />
+            />
         </svg>
       </div>
+    </div>
     </nav>
   );
 };
--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -7,53 +7,53 @@ import carousel3 from "../../assets/carousel-3.png";
 const Carousel = () => {
   return (
     <div className="d-sm-none d-md-block d-none d-sm-block">
-    <div className="Carousel ">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={16}
-        height={16}
-        fill="currentColor"
-        className="bi bi-arrow-left-circle-fill"
-        viewBox="0 0 16 16"
-      >
-        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
-      </svg>
+      <div className="Carousel ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={16}
+          height={16}
+          fill="currentColor"
+          className="bi bi-arrow-left-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+        </svg>
 
-      <div className="carouselItem carouselItem1">
-        <img src={carousel1} alt="carousel-img" className="carousel-img" />
-        <div className="carousel-content-wrapper">
-          <p className="color-secandary">Take a quiz!</p>
-          <p className="font-weight-bold h5">Learn and earn $CKB</p>
+        <div className="carouselItem carouselItem1">
+          <img src={carousel1} alt="carousel-img" className="carousel-img" />
+          <div className="carousel-content-wrapper">
+            <p className="color-secandary">Take a quiz!</p>
+            <p className="font-weight-bold h5">Learn and earn $CKB</p>
+          </div>
         </div>
-      </div>
-      <div className="carouselItem carouselItem2">
-        <img src={carousel2} alt="carousel-img" className="carousel-img" />
-        <div className="carousel-content-wrapper">
-          <p className="color-secandary">Portfolio 🔥</p>
-          <p className="font-weight-bold h5">
+        <div className="carouselItem carouselItem2">
+          <img src={carousel2} alt="carousel-img" className="carousel-img" />
+          <div className="carousel-content-wrapper">
+            <p className="color-secandary">Portfolio 🔥</p>
+            <p className="font-weight-bold h5">
+              Track your trades in one place, not all over the place
+            </p>
+          </div>
+        </div>
+        <div className="carouselItem carouselItem3">
+          <img src={carousel3} alt="carousel-img" className="carousel-img" />
+          <div className="carousel-content-wrapper">
+            <p className="color-secandary">Portfolio</p>
             Track your trades in one place, not all over the place
-          </p>
+            <p className="font-weight-bold h5"></p>
+          </div>
         </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={16}
+          height={16}
+          fill="currentColor"
+          className="bi bi-arrow-right-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+        </svg>
       </div>
-      <div className="carouselItem carouselItem3">
-        <img src={carousel3} alt="carousel-img" className="carousel-img" />
-        <div className="carousel-content-wrapper">
-          <p className="color-secandary">Portfolio</p>
-          Track your trades in one place, not all over the place
-          <p className="font-weight-bold h5"></p>
-        </div>
-      </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={16}
-        height={16}
-        fill="currentColor"
-        className="bi bi-arrow-right-circle-fill"
-        viewBox="0 0 16 16"
-      >
-        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-      </svg>
-    </div>
     </div>
   );
 };
