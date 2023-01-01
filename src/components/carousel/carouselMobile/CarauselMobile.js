487bf8c772b5af4415295115205865a3862aef3a
Kaushik G Bhat <kaushikgbhat@Kaushiks-Mac-mini.local>
2023-01-01 12:25:42 +0530

    added mobile carousel with dots

--- src/components/carousel/carouselMobile/CarauselMobile.js
+++ src/components/carousel/carouselMobile/CarauselMobile.js
@@ -6,16 +6,25 @@ const CarauselMobile = () => {
   return (
     <div className="carousel-mobile">
       <div className="carousel-mobile-flex">
-        <img src={carousel1} alt="carousel__img" className="carousel__img__mobile" />
+        <img
+          src={carousel1}
+          alt="carousel__img"
+          className="carousel__img__mobile"
+        />
         <div className="carousel__content__wrapper__mobile ">
           <p className="text-secondary carousel__p1__mobile">Take a quiz!</p>
           <p className="carousel__p2__mobile">Learn and earn $CKB</p>
         </div>
       </div>
+      <div className="carousel__mobile__navigation">
+     <div className="radio-wrapper">
+      <input className="radio__flex__mobile" type="radio" name="" id="" />
+      <input className="radio__flex__mobile" type="radio" name="" id="" checked/>
+      <input className="radio__flex__mobile" type="radio" name="" id="" />
+      <input className="radio__flex__mobile" type="radio" name="" id="" />
 
-
-
-
+     </div>
+      </div>
     </div>
   );
 };
