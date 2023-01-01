0abb6e0a989fb149766225ea139d34a7cfaf9e01
Kaushik G Bhat <kaushikgbhat@Kaushiks-Mac-mini.local>
2023-01-01 19:53:34 +0530

    pagination done

--- src/components/table/tablemobile/TableMobile.js
+++ src/components/table/tablemobile/TableMobile.js
@@ -6,11 +6,7 @@ const TableMobile = ({
   name,
   symbol,
   price,
-  oneDayPriceChange,
-  sevenDayPriceChange,
-  marketCap,
-  volume,
-  circulatingSupply,
+  oneDayPriceChange
 }) => {
   return (
     <>
@@ -66,84 +62,9 @@ const TableMobile = ({
             {Math.abs(oneDayPriceChange).toFixed(2)}%
           </div>
         </td>
-
-        {/* <td className={sevenDayPriceChange >= 0 ? "text-success" : "text-danger"}>
-          <div className="d-flex align-items-center">
-          {sevenDayPriceChange >= 0 ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-caret-up-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-caret-down-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-              </svg>
-            )}
-            {Math.abs(sevenDayPriceChange).toFixed(2)}%
-          </div>
-        </td>
-        <td className="font-weight-bold">${marketCap}</td>
-        <td>
-          <div className="d-flex flex-column">
-            <span className="font-weight-bold">${volume}</span>
-            <span className="text-muted">932,071 {symbol}</span>
-          </div>
-        </td> */}
-        {/* 
-        <td>
-          <div className="d-flex flex-column">
-            <span className="font-weight-bold">${circulatingSupply}</span>
-            <div
-              className="progress "
-              style={{ height: "8px" }}
-              role="progressbar"
-              aria-label="Warning example"
-              aria-valuenow={75}
-              aria-valuemin={0}
-              aria-valuemax={100}
-            >
-              <div
-                className="progress-bar bg-secondary"
-                style={{ width: "75%" }}
-              />
-            </div>
-          </div>
-        </td> */}
-        {/* 
-        <td>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-three-dots-vertical"
-            viewBox="0 0 16 16"
-          >
-            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-          </svg>
-        </td> */}
       </tr>
     </>
   );
 };
 
 export default TableMobile;
-
-// <th scope="col">7D</th>
-//       <th scope="col">MARKRT CAP</th>
-//       <th scope="col">VOLUME(24H)</th>
-//       <th scope="col">CIRCULATING SUPPLY</th>
-//       <th scope="col"></th>
