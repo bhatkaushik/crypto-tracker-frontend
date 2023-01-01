65f4299a806aa87118b4050afab7e05a7efec39b
Kaushik G Bhat <kaushikgbhat@Kaushiks-Mac-mini.local>
2023-01-01 11:34:39 +0530

    added mobile carousel

--- src/components/carousel/carouselMobile/CarauselMobile.js
+++ src/components/carousel/carouselMobile/CarauselMobile.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
 import carousel1 from "../../../assets/carousel-1.png";
-import carousel2 from "../../../assets/carousel-2.png";
-import carousel3 from "../../../assets/carousel-3.png";
-import './carouselMobile.css'
+import "./carouselMobile.css";
 
 const CarauselMobile = () => {
   return (
     <div className="carousel-mobile">
+      <div className="carousel-mobile-flex">
+        <img src={carousel1} alt="carousel__img" className="carousel__img__mobile" />
+        <div className="carousel__content__wrapper__mobile ">
+          <p className="text-secondary carousel__p1__mobile">Take a quiz!</p>
+          <p className="carousel__p2__mobile">Learn and earn $CKB</p>
+        </div>
+      </div>
+
+
 
 
     </div>
-  )
-}
+  );
+};
 
-export default CarauselMobile
+export default CarauselMobile;
